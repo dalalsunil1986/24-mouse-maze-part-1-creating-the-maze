c434aacbbb003054caed56c239a127ad0c70d09f
Mark Tellez <mark.tellez@icloud.com>
2018-08-13 17:09:03 -0500

    wire up redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/app'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import App from './components/app'
+import store from './config/store'
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>, document.getElementById('root'))
